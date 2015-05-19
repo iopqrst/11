09dd78b390075f1966cfe88f8de8dda36ea196ec
iopqrst <opqrst@163.com>
2015-05-19 18:50:46 +0800

    回去接着来

--- app/js/services.js
+++ app/js/services.js
@@ -5,11 +5,11 @@ var phonecatServices = angular.module('phonecatServices', ['ngResource']);
 
 phonecatServices.factory('Phone', ['$resource',
 	function($resource) {
-		return $resource('phones/:phoneId.json', {}, {
+		return $resource('phones/:phoneId', {}, {
 			query: {
 				method: 'GET',
 				params: {
-					phoneId: 'phones'
+					phoneId2: 'phones'
 				},
 				isArray: true
 			}
