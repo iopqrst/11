17a16f8fbb843dd4a8b036b4ccfcbbf981a56749
iopqrst <opqrst@163.com>
2016-01-26 18:28:30 +0800

    no message

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,12 +4,12 @@
 
 var phonecatControllers = angular.module('phonecatControllers', []);
 
-phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-	function($scope, Phone) {
-		$scope.phones = Phone.query();
-		$scope.orderProp = 'age';
-	}
-]);
+//phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+//	function($scope, Phone) {
+//		$scope.phones = Phone.query();
+//		$scope.orderProp = 'age';
+//	}
+//]);
 
 phonecatControllers.controller('MyPhoneListCtrl', ['$scope', 'MyPhone',
 	function($scope, MyPhone) {
@@ -23,20 +23,23 @@ phonecatControllers.controller('MyPhoneListCtrl', ['$scope', 'MyPhone',
 	}
 ]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-	function($scope, $routeParams, Phone) {
-		$scope.phone = Phone.get({
-			phoneId: $routeParams.phoneId
-		}, function(phone) {
-			console.info("phone ---> " + JSON.stringify(phone));
-			$scope.mainImageUrl = phone.images[0];
-		});
-
-		$scope.setImage = function(imageUrl) {
-			$scope.mainImageUrl = imageUrl;
-		}
-	}
-]);
+//phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
+//	function($scope, $routeParams, Phone) {
+//		
+//		console.info ("$ = " + $routeParams.phoneId);
+//		
+//		$scope.phone = Phone.get({
+//			phoneId: $routeParams.phoneId
+//		}, function(phone) {
+//			console.info("phone ---> " + JSON.stringify(phone));
+//			$scope.mainImageUrl = phone.images[0];
+//		});
+//
+//		$scope.setImage = function(imageUrl) {
+//			$scope.mainImageUrl = imageUrl;
+//		}
+//	}
+//]);
 
 phonecatControllers.controller('MyPhoneDetailCtrl', ['$scope', '$routeParams', 'MyPhone',
 	function($scope, $routeParams, MyPhone) {
--- app/js/services.js
+++ app/js/services.js
@@ -3,19 +3,19 @@
 /* Services */
 var phonecatServices = angular.module('phonecatServices', ['ngResource']);
 
-phonecatServices.factory('Phone', ['$resource',
-	function($resource) {
-		return $resource('phones/:phoneId.json', {}, {
-			query: {
-				method: 'GET',
-				params: {
-					phoneId: 'phones-list'
-				},
-				isArray: true
-			}
-		});
-	}
-]);
+//phonecatServices.factory('Phone', ['$resource',
+//	function($resource) {
+//		return $resource('phones/:phoneId.json', {}, {
+//			query: {
+//				method: 'GET',
+//				params: {
+//					phoneId: 'phones-list'
+//				},
+//				isArray: true
+//			}
+//		});
+//	}
+//]);
 
 phonecatServices.factory('MyPhone', ['$resource',
 	function($resource) {
