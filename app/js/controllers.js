5644177a39571eb09a94d920a7794a1e5cb12368
iopqrst <opqrst@163.com>
2016-03-16 22:37:41 +0800

    no message

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,42 +4,17 @@
 
 var phonecatControllers = angular.module('phonecatControllers', []);
 
-//phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
-//	function($scope, Phone) {
-//		$scope.phones = Phone.query();
-//		$scope.orderProp = 'age';
-//	}
-//]);
-
 phonecatControllers.controller('MyPhoneListCtrl', ['$scope', 'MyPhone',
 	function($scope, MyPhone) {
 		MyPhone.query({
 			phoneId: 'phones-list.json'
-		}, function(_phones) {
-			console.info('phones size = ' + _phones.length);
-			$scope.phones = _phones;
+		}, function(phones) {
+			console.info('phones size = ' + phones.length);
+			$scope.phones = phones;
 		});
 		$scope.orderProp = 'age';
 	}
 ]);
-
-//phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
-//	function($scope, $routeParams, Phone) {
-//		
-//		console.info ("$ = " + $routeParams.phoneId);
-//		
-//		$scope.phone = Phone.get({
-//			phoneId: $routeParams.phoneId
-//		}, function(phone) {
-//			console.info("phone ---> " + JSON.stringify(phone));
-//			$scope.mainImageUrl = phone.images[0];
-//		});
-//
-//		$scope.setImage = function(imageUrl) {
-//			$scope.mainImageUrl = imageUrl;
-//		}
-//	}
-//]);
 
 phonecatControllers.controller('MyPhoneDetailCtrl', ['$scope', '$routeParams', 'MyPhone',
 	function($scope, $routeParams, MyPhone) {
--- app/js/services.js
+++ app/js/services.js
@@ -3,20 +3,6 @@
 /* Services */
 var phonecatServices = angular.module('phonecatServices', ['ngResource']);
 
-//phonecatServices.factory('Phone', ['$resource',
-//	function($resource) {
-//		return $resource('phones/:phoneId.json', {}, {
-//			query: {
-//				method: 'GET',
-//				params: {
-//					phoneId: 'phones-list'
-//				},
-//				isArray: true
-//			}
-//		});
-//	}
-//]);
-
 phonecatServices.factory('MyPhone', ['$resource', function($resource) {
 	return $resource('phones/:phoneId');
 }]);
