fc5a09d1ac0078f959fbcb7382a6c4ddbccd66ac
iopqrst <opqrst@163.com>
2016-05-02 14:39:10 +0800

    no message

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -2,11 +2,11 @@
 
 describe('service', function() {
 
-  // load modules
-  beforeEach(module('phonecatApp'));
+	// load modules
+	beforeEach(module('phonecatApp'));
 
-  // Test service availability
-  it('check the existence of Phone factory', inject(function(Phone) {
-      expect(Phone).toBeDefined();
-    }));
+	// Test service availability
+	it('check the existence of Phone factory', inject(function(Phone) {
+		expect(Phone).toBeDefined();
+	}));
 });
